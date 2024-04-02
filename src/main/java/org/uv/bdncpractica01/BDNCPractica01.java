f5cd58ccc8933997a7bae6fea8e8ab20dbb02f04
josemmanuel <emmanuelriv15@gmail.com>
2024-04-02 12:44:21 -0600

    add base code

---
+++ src/main/java/org/uv/bdncpractica01/BDNCPractica01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package org.uv.bdncpractica01;
+
+import org.uv.dapp02practica01.ConexionDB;
+
+/**
+ *
+ * @author JMRes
+ */
+public class BDNCPractica01 {
+
+    public static void main(String[] args) {
+        ConexionDB con = ConexionDB.getInstance();
+    }
+}
